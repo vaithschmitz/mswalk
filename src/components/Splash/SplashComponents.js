e51179d7dc84552e4b9c838382249336113ddcb8
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-08-27 16:32:42 +0100

    .

--- src/components/Splash/SplashComponents.js
+++ src/components/Splash/SplashComponents.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import './SplashComponents.css'
 
-export const KilometerButton = (props) => <div id='KilometerButton'>{props.kilometer}</div>
+export const KilometerButton = (props) => <div class='KilometerButton'>{props.kilometer}</div>
