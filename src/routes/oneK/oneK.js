70aabb54af237d6773a55219a802cb323c53a85c
vaithschmitz <vaithschmitz@googlemail.com>
2019-08-28 13:41:27 +0100

    list styling

--- src/routes/oneK/oneK.js
+++ src/routes/oneK/oneK.js
@@ -18,27 +18,36 @@ export default function OneK(){
         if(navIs === 'list'){
             return(
                 <div id='oneK-list'>
+                <List number={'1'} text={'And we’re off! Walk through the start line and straight ahead towards marshal point 1.'}/>
+                <List number={'2'} text={'At marshal point 1, turn right towards the lake, keeping the lake on your left.'}/>
+                <List number={'3'} text={'Continue on the path, passing the Pump House Gallery on your left. Did you know? The Pump House Gallery is housed in a former Victorian industrial pump house used for the irrigation and fountains of Battersea Park.'}/>
+                <List number={'4'} text={'When you get to marshal point 2, take the middle of the three paths, keeping the football pitches on your left.'}/>
+                <List number={'5'} text={'At the path crossroads, turn right onto the home straight and you are nearly there.'}/>
+                <List number={'6'} text={'You have now finished. Well done! Now relax and enjoy the rest of your day.'}/>
 
-                    <List number={'1'} text={'And we’re off! Walk through the start line and straight ahead towards marshal point 1.'}/>
-                    {/* <p>
-                        1. And we’re off! Walk through the start line and straight ahead towards marshal point 1. 
-                        <br/>
-                        <br/>
-                        2. At marshal point 1, turn right towards the lake, keeping the lake on your left. 
-                        <br/>
-                        <br/>
-                        3. Continue on the path, passing the Pump House Gallery on your left. Did you know? The Pump House Gallery is housed in a former Victorian industrial pump house used for the irrigation and fountains of Battersea Park. 
-                        <br/>
-                        <br/>
-                        4. When you get to marshal point 2, take the middle of the three paths, keeping the football pitches on your left. 
-                        <br/>
-                        <br/>
-                        5. At the path crossroads, turn right onto the home straight and you are nearly there. 
-                        <br/>
-                        <br/>
-                        6. You have now finished. Well done! Now relax and enjoy the rest of your day. 
-                    </p> */}
                 </div>
+                // <div id='oneK-list'>
+
+                //     <List number={'1'} text={'And we’re off! Walk through the start line and straight ahead towards marshal point 1.'}/>
+                //     <p>
+                //         1. And we’re off! Walk through the start line and straight ahead towards marshal point 1. 
+                //         <br/>
+                //         <br/>
+                //         2. At marshal point 1, turn right towards the lake, keeping the lake on your left. 
+                //         <br/>
+                //         <br/>
+                //         3. Continue on the path, passing the Pump House Gallery on your left. Did you know? The Pump House Gallery is housed in a former Victorian industrial pump house used for the irrigation and fountains of Battersea Park. 
+                //         <br/>
+                //         <br/>
+                //         4. When you get to marshal point 2, take the middle of the three paths, keeping the football pitches on your left. 
+                //         <br/>
+                //         <br/>
+                //         5. At the path crossroads, turn right onto the home straight and you are nearly there. 
+                //         <br/>
+                //         <br/>
+                //         6. You have now finished. Well done! Now relax and enjoy the rest of your day. 
+                //     </p>
+                // </div>
             )
         }
         if(navIs === 'quiz'){
