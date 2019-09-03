d06b9a3fac90e0a8d9cf84ee16d2ddf7ec7a8072
vaithschmitz <vaithschmitz@googlemail.com>
2019-09-03 15:13:18 +0100

    testing direction service

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,8 +1,38 @@
-import React from 'react'
+/* global google */
+import React, {useState, useEffect} from 'react'
 import { compose, withProps } from "recompose"
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, DirectionsRenderer } from "react-google-maps"
 
 let REACT_APP_MAPSKEY = process.env.REACT_APP_MAPSKEY
+
+function MyDirectionsRenderer(props) {
+    const [directions, setDirections] = useState(null);
+    const { origin, destination, travelMode } = props;
+  
+    useEffect(() => {
+      const directionsService = new google.maps.DirectionsService();
+      directionsService.route(
+        {
+          origin: new google.maps.LatLng(origin.lat, origin.lng),
+          destination: new google.maps.LatLng(destination.lat, destination.lng),
+          travelMode: travelMode
+        },
+        (result, status) => {
+          if (status === google.maps.DirectionsStatus.OK) {
+            setDirections(result);
+          } else {
+            console.error(`error fetching directions ${result}`);
+          }
+        }
+      );
+    }, [directions]);
+  
+    return (
+      <React.Fragment>
+        {directions && <DirectionsRenderer directions={directions} />}
+      </React.Fragment>
+    );
+  }
 
 const Map = compose(
 withProps({
@@ -19,6 +49,11 @@ withProps({
     defaultCenter={{ lat: props.lat, lng: props.lng }}
 >
     {props.isMarkerShown && <Marker position={{ lat: props.lat, lng: props.lng  }} />}
+    <MyDirectionsRenderer
+      origin={{ lat: props.lat, lng: props.lng }}
+      destination={{ lat: 51.5510573, lng: -0.22638 }}
+      travelMode={google.maps.TravelMode.WALKING}
+    />
 </GoogleMap>
 )
 
